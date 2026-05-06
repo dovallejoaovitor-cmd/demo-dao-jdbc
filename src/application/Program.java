d70bc14ff19e58da4f4a77b0a51d74280e3d9212
dovallejoaovitor-cmd <dovallejoaovitor@gmail.com>
2026-05-06 12:44:00 -0300

    Creating class Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entites.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department dp = new Department(1, "Books");
 		System.out.println(dp);
+		
+		Seller seller = new Seller(2, "Bobson da Silva", "Bobson@gmail.com", new Date(), 2000.0, dp);
+		System.out.println(seller);
 	}
 
 }
