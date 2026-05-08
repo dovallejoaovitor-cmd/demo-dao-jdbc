888dcf682419b2a7c0a145d5847ce1e4a036d009
dovallejoaovitor-cmd <dovallejoaovitor@gmail.com>
2026-05-07 22:03:13 -0300

    Creating findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,6 +25,12 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("=== TEST 3: seller findAll ===");
+		List<Seller> list2 = sellerDao.findAll();
+		for (Seller obj : list2) {
+			System.out.println(obj);
+		}
 	}
 
 }
